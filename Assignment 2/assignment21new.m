b8cad7c3ca75eae06162264f9b1735f1fbcc6841
anbl9574 <anton.blahomildton@gmail.com>
2024-02-08 14:20:57 +0100

    Your commit message here

--- Assignment 2/assignment21new.m
+++ Assignment 2/assignment21new.m
@@ -1,12 +1,15 @@
+clc
+clearvars
+close all
 
-M=2^kpv;
-Es=1;
+load("Bitstream.mat");
+bitstream = estimatedBitStream;
+%randi([0 1], 100, 1);
+M=4;
+Es=4;
 k = log2(M);
 transmittedSignal = MyMPAM(bitstream, M, Es);
-
-
-
-
+plot(transmittedSignal(1:4000))
 
 function Signal = MyMPAM(bitstream,M,Es)
     k = log2(M);
@@ -14,13 +17,13 @@ function Signal = MyMPAM(bitstream,M,Es)
     symbols=bi2de(symbolMatrix, 'left-msb') + 1;
     % Map symbols to amplitudes (equally and symmetrically spaced)
     amplitudeLevels = linspace(-sqrt(Es), sqrt(Es), M);
+    %disp(amplitudeLevels)
     signalAmplitudes = amplitudeLevels(symbols);
+    %disp(signalAmplitudes)
     % Create the transmitted signal
     Signal = repelem(signalAmplitudes, k);
-
-
+%     for i = 1:length(Signal)
+%         Es_calc =+ Signal(i)^2;
+%     end
+%     disp(Es_calc)
 end 
-
-
-
-function 
