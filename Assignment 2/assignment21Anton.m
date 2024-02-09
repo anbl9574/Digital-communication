045b58b4efe53c73a2e3d4904bfcb46368c8103c
anbl9574 <anton.blahomildton@gmail.com>
2024-02-09 11:30:08 +0100

    Your commit message here

--- Assignment 2/assignment21Anton.m
+++ Assignment 2/assignment21Anton.m
@@ -4,16 +4,21 @@ close all
 
 load("Bitstream.mat");
 bitstream = estimatedBitStream;
+
+transmittedBitstream=bitstream;
 %randi([0 1], 100, 1);
 M=4;
 Es=1;
-%transmittedSignal = MyMPAM(bitstream, M, Es);
+T=100;
+receivedSignal = MyMPAM(bitstream, M, Es);
+plot(receivedSignal(1:10000))%transmitted
+[estimatedBitstream, BER] = DemodulateMPAM(receivedSignal,M,Es,transmittedBitstream)
 
-%function Signal = MyMPAM(bitstream,M,Es)
-    d = sqrt(3*Es/(M^2-1))
+function receivedSignal = MyMPAM(bitstream,M,Es)
+    d = sqrt(3*Es/(M^2-1));
     k = log2(M);
     symbolMatrix = reshape(bitstream, k, length(bitstream)/k)';
-    symbols=bi2de(symbolMatrix, 'left-msb') + 1;
+    symbols=bi2de(symbolMatrix, 'left-msb') + Es;
     % Map symbols to amplitudes (equally and symmetrically spaced)
     amplitudeLevels = linspace(-(M-1)*d, (M-1)*d, M);
     %disp(amplitudeLevels)
@@ -25,11 +30,11 @@ Es=1;
     for i = 1:length(amplitudeLevels)
         E = E + amplitudeLevels(i)^2;
     end
-    E = E/4
+    E = E/4;
     if length(Signaltransmit)==length(bitstream)
         transmitSignal=Signaltransmit;
     else 
-        transmitSignal=padarray(Signaltransmit,[0,length(bitstream)-length(Signaltransmit)],'post')
+        transmitSignal=padarray(Signaltransmit,[0,length(bitstream)-length(Signaltransmit)],'post');
     end 
     % Rectangular pulse shaping
     T=100;
@@ -37,8 +42,35 @@ Es=1;
     shapedSignal = rectpulse(transmitSignal, sampleRate);
 
     % Return shaped signal
-    Signal = shapedSignal;
+    receivedSignal=shapedSignal;
 
 
-%end 
-plot(Signal(1:500))%transmitted
+end 
+
+function [estimatedBitstream, BER] = DemodulateMPAM(receivedSignal, M, Es, transmittedBitstream, T)
+
+        % Reshape received signal into matrix with T elements per rowT=100;
+         T=100;
+         d = sqrt(3*Es/(M^2-1));
+         k = log2(M);
+
+         receivedMatrix = reshape(receivedSignal, T, length(receivedSignal)/T)';
+         amplitudeLevels = linspace(-(M-1)*d, (M-1)*d, M);
+
+
+         % Estimate symbols from received samples
+         estimatedSymbols = zeros(size(receivedMatrix, 1), 1);
+         for i = 1:size(receivedMatrix, 1)
+            [~, index] = min(receivedMatrix(i, 1:4) -  linspace(-(M-1)*d, (M-1)*d, M)); %Something is wrong here
+            estimatedSymbols(i) = index - 1;  
+
+            % Convert symbols to bitstream
+            estimatedSymbolMatrix = de2bi(estimatedSymbols, k, 'left-msb');
+            estimatedBitstream = reshape(estimatedSymbolMatrix', 1, []);
+
+            % Calculate Bit Error Rate (BER)
+            %numErrors = sum(estimatedBitstream ~= transmittedBitstream); %Wrong here
+            %BER = numErrors / length(transmittedBitstream);
+
+         end
+end 
