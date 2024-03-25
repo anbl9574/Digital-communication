428f916e8899e84ab6f18b6cb18fbfdbcf9d0373
gulo8264 <147599546+gulo8264@users.noreply.github.com>
2024-03-25 08:04:46 +0100

    Ass4 klar

--- Assignment4/Assignment4.m
+++ Assignment4/Assignment4.m
@@ -5,20 +5,11 @@ clc
 load("Bitstream4bit.mat");
 c = estimatedBitStream;
 
-c1 = encodeHAM74(c);
-c2 = encodeHAM1511(c);
-e1 = decodeHAM74(c1);
-e2 = decodeHAM1511(c2);
-e2 = e2(1:length(c));
-est = [c;e1;e2];
-
-ie1 = isequal(c,e1);
-ie2 = isequal(c,e2);
-
+HammingTest(c)
 BERplot(c)
 
 %Functions
-% function c = OWNencodeHAM74(b)
+% function c = OwnEncodeHAM74(b)
 %     % Padding
 %     p = mod(length(b),4);
 %     if (p > 0)
@@ -49,8 +40,8 @@ function c = encodeHAM74(b)
     end
     % Encoding
     c = [];
+    [~,G] = hammgen(3);
     for i = 1:length(b)/4
-        [~,G] = hammgen(3);
         d = b(4*(i-1)+1:4*(i-1)+4);
         t = d*G;
         for j = 1:7 
@@ -69,8 +60,8 @@ function c = encodeHAM1511(b)
     end
     % Encoding
     c = [];
+    [~,G] = hammgen(4);
     for i = 1:length(b)/11
-        [~,G] = hammgen(4);
         d = b(11*(i-1)+1:11*(i-1)+11);
         t = d*G;
         for j = 1:15
@@ -83,8 +74,8 @@ end
 function bEst = decodeHAM74(c)
     % Decoding
     bEst = [];
+    H = hammgen(3);
     for i = 1:length(c)/7
-        H = hammgen(3);
         d = c(7*(i-1)+1:7*(i-1)+7);
         s = d*H';
         for j = 1:3
@@ -93,7 +84,7 @@ function bEst = decodeHAM74(c)
         if isequal(s,[0,0,0])
             bEst = [bEst,d(4:7)];
         else
-            [~,idx] = ismember(s,H');
+            [~,idx] = ismember(s,H','rows');
             d(idx) = ~d(idx);
             bEst = [bEst,d(4:7)];
         end
@@ -103,8 +94,8 @@ end
 function bEst = decodeHAM1511(c)
     % Decoding
     bEst = [];
+    H = hammgen(4);
     for i = 1:length(c)/15
-        H = hammgen(4);
         d = c(15*(i-1)+1:15*(i-1)+15);
         s = d*H';
         for j = 1:4
@@ -113,7 +104,7 @@ function bEst = decodeHAM1511(c)
         if isequal(s,[0,0,0,0])
             bEst = [bEst,d(5:15)];
         else
-            [~,idx] = ismember(s,H');
+            [~,idx] = ismember(s,H','rows');
             d(idx) = ~d(idx);
             bEst = [bEst,d(5:15)];
         end
@@ -155,33 +146,119 @@ for i = 1:length(r)
 end
 end
 
+function HammingTest(c)
+    c1 = encodeHAM74(c);
+    c2 = encodeHAM1511(c);
+    e1 = decodeHAM74(c1);
+    e2 = decodeHAM1511(c2);
+    e2 = e2(1:length(c));
+    
+    x4 = (1:44);
+    x7 = (1:77);
+    x11 = (1:44);
+    x15 = (1:60);
+    
+    figure;
+    % First subplot
+    subplot(3, 1, 1);
+    stairs(x4, c(1:44), 'b');
+    title('Original Bits');
+    xlabel('Bits');
+    ylabel('Value');
+    ylim([-0.1,1.1])
+    xlim([1,44])
+
+    % Second subplot
+    subplot(3, 1, 2);
+    stairs(x7, c1(1:77), 'r');
+    title('Hamming (7,4) Coded Bits');
+    xlabel('Bits');
+    ylabel('Value');
+    ylim([-0.1,1.1])
+    xlim([1,77])
+
+    % Third subplot
+    subplot(3, 1, 3);
+    stairs(x4, e1(1:44), 'b');
+    title('Decoded Bits');
+    xlabel('Bits');
+    ylabel('Value');
+    ylim([-0.1,1.1])
+    xlim([1,44])
+    
+    figure;
+    title('Hamming(15,11)');
+    % First subplot
+    subplot(3, 1, 1);
+    stairs(x11, c(1:44), 'b');
+    title('Original Bits');
+    xlabel('Bits');
+    ylabel('Value');
+    ylim([-0.1,1.1])
+    xlim([1,44])
+
+    % Second subplot
+    subplot(3, 1, 2);
+    stairs(x15, c2(1:60), 'r');
+    title('Hamming (15,11) Coded Bits');
+    xlabel('Bits');
+    ylabel('Value');
+    ylim([-0.1,1.1])
+    xlim([1,60])
+
+    % Third subplot
+    subplot(3, 1, 3);
+    stairs(x11, e2(1:44), 'b');
+    title('Decoded Bits');
+    xlabel('Bits');
+    ylabel('Value');
+    ylim([-0.1,1.1])
+    xlim([1,44])
+end
 
 function BERplot(c)
-% Repeating vector for larger amount of samples
-%c = [c,c,c,c];%,c,c,c,c,c,c,c,c,c,c,c,c];
+%c = c(1:1000);
+%c = [c,c,c,c];
+%c = [c,c,c,c];
 
-% Noise variance vector
-No = logspace(-1.5,1,15);
+%Theoretical BPSK BER
+Eb_No = logspace(-1,3,100);
+Eb = 1;
+No = Eb./Eb_No;
+NO = 10.^(No / 10);
+BERBPSKTHEO = qfunc(sqrt(2*Eb_No));
 
-%testing
 
+%Theoretical BPSK with HAM74 BER
+n = 7;
+k = 4;
+p = qfunc(sqrt(2*Eb_No));
+BERBPSKTHEO7 = p-p.*(1-p).^(n-1);
+%0.5.*erfc(sqrt((k/n).*Eb_No)).*(1- (1-0.5.*erfc(sqrt((k/n).*Eb_No))).^(n-1));
+
+%Theoretical BPSK with HAM1511 BER
+n = 15;
+k = 11;
+BERBPSKTHEO15 = p-p.*(1-p).^(n-1);
+%qfunc(sqrt(2*Eb_No)).*(1 - (1-0.5.*erfc(sqrt((k/n).*Eb_No))).^(n-1));
+
+%Empirical BPSK with HAM74 BER
 BERBPSK7 = (1:length(No));
-for i = 1:length(No)
-    e = encodeHAM74(c);
-    s = mapontoBPSK(e);
-    r = channel2(s,No(i));
-    h = detectBPSK(r);
+e = encodeHAM74(c);
+s = mapontoBPSK(e);
+for i = 1:length(No)     
+    n = channel2(s,No(i));
+    h = detectBPSK(n);
     b = decodeHAM74(h);
-    
     % Claculate and store BER for plotting
-    BER = sum(c ~= b)/length(c);
+    BER = sum(abs(c - b))/length(c);
     BERBPSK7(i) = BER;
 end
 
 BERBPSK15 = (1:length(No));
+e = encodeHAM1511(c);
+s = mapontoBPSK(e);
 for i = 1:length(No)
-    e = encodeHAM1511(c);
-    s = mapontoBPSK(e);
     r = channel2(s,No(i));
     h = detectBPSK(r);
     b = decodeHAM1511(h);
@@ -192,29 +269,44 @@ for i = 1:length(No)
     BERBPSK15(i) = BER;
 end
 
-% Calculating theoretical BER
-Es = 1;
-Eb = Es/log2(16);
-EsNo = Es./No;
-EbNo = Eb./No;
-EbNo_dB = 10*log10(EbNo);
-
-p = qfunc(sqrt(2*EbNo));
-
-BERBPSKTHEO7 = p-p.*(1-p).^2;%qfunc(sqrt(2*EsNo));
-BERBPSKTHEO15 = p-p.*(1-p).^3;
-
-% Plotting BER
+% Plotting Theoretical BER
 figure;
-semilogy(EbNo_dB, BERBPSK7, '.-');
+semilogy(db(Eb_No,'power'),BERBPSKTHEO7);
 hold on;
-semilogy(EbNo_dB, BERBPSK15, '.-');
-semilogy(EbNo_dB, BERBPSKTHEO7, '.-');
-semilogy(EbNo_dB, BERBPSKTHEO15, '.-');
-title('BER vs SNR for BPSK');
+semilogy(db(Eb_No,'power'),BERBPSKTHEO15);
+semilogy(db(Eb_No,'power'),BERBPSKTHEO);
+title('Theoretical BER for BPSK using HAM74 and HAM1511')
 xlabel('Signal Noise Ratio (SNR [dB])');
 ylabel('Bit Error Rate (BER)');
-ylim([10^-5,1]);
+ylim([10^-7,1]);
 grid on;
-legend('Hamming(7,4)','Hamming(15,11)', 'Theoretical BPSK', 'Location', 'southwest');
+legend('Hamming (7,4)','Hamming (15,11)', 'Uncoded BPSK', 'Location', 'southwest');
+
+% Plotting Empirical BER for HAM74
+figure;
+semilogy(db(Eb_No,'power'),BERBPSK7, 'bx');
+hold on;
+semilogy(db(Eb_No,'power'),BERBPSKTHEO7);
+semilogy(db(Eb_No,'power'),BERBPSKTHEO);
+title('Empirical BER for BPSK using HAM74');
+xlabel('Signal Noise Ratio (SNR [dB])');
+ylabel('Bit Error Rate (BER)');
+ylim([10^-7,1]);
+grid on;
+legend('Empirical Hamming(7,4)', 'Theoretical Hamming(7,4)', 'Uncoded BPSK', 'Location', 'southwest');
+
+% Plotting Empirical BER for HAM1511
+figure;
+semilogy(db(Eb_No,'power'),BERBPSK15, 'bx');
+hold on;
+semilogy(db(Eb_No,'power'),BERBPSKTHEO15);
+semilogy(db(Eb_No,'power'),BERBPSKTHEO);
+title('Empirical BER for BPSK using HAM1511');
+xlabel('Signal Noise Ratio (SNR [dB])');
+ylabel('Bit Error Rate (BER)');
+ylim([10^-7,1]);
+grid on;
+legend('Empirical Hamming(15,11)', 'Theoretical Hamming(15,11)', 'Uncoded BPSK', 'Location', 'southwest');
+
+
 end
